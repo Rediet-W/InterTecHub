9fd34fe84468cac133862238d941bbebad1345f2
Rediet_W <vivezlaviequetuaimes@gmail.com>
2024-11-28 00:21:16 +0300

    database issue fixed

--- src/db/db.js
+++ src/db/db.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connection successful");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1);
